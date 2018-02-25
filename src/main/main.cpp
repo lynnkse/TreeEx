ab9eb18b540da7d47366782e35516e4508a7a327
Lynnkse <lynnkse@gmail.com>
2018-02-25 07:24:49 -0500

    more tests

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -24,7 +24,60 @@ Analyzer analyzer(100);
 ASSERT_THAT(analyzer.GetResult(tree) == 0);
 END_UNIT
 
+UNIT(list)
+Analyzer analyzer(100);
+Node tree(1);
+tree.AddRight(new Node(2));
+tree.GetRight()->AddRight(new Node(3));
+tree.GetRight()->GetRight()->AddRight(new Node(4));
+tree.GetRight()->GetRight()->GetRight()->AddRight(new Node(5));
+tree.GetRight()->GetRight()->GetRight()->GetRight()->AddRight(new Node(5));
+ASSERT_THAT(analyzer.GetResult(&tree) == 5);
+END_UNIT
+
+UNIT(list1)
+Analyzer analyzer(100);
+Node tree(1);
+tree.AddRight(new Node(2));
+tree.GetRight()->AddRight(new Node(3));
+tree.GetRight()->GetRight()->AddRight(new Node(4));
+tree.GetRight()->GetRight()->GetRight()->AddRight(new Node(5));
+tree.GetRight()->GetRight()->GetRight()->GetRight()->AddRight(new Node(6));
+ASSERT_THAT(analyzer.GetResult(&tree) == 6);
+END_UNIT
+
+UNIT(list2)
+Analyzer analyzer(100);
+Node tree(1);
+tree.AddRight(new Node(1));
+tree.GetRight()->AddRight(new Node(1));
+tree.GetRight()->GetRight()->AddRight(new Node(1));
+tree.GetRight()->GetRight()->GetRight()->AddRight(new Node(1));
+tree.GetRight()->GetRight()->GetRight()->GetRight()->AddRight(new Node(1));
+ASSERT_THAT(analyzer.GetResult(&tree) == 1);
+END_UNIT
+
+UNIT(all_same)
+Analyzer analyzer(1000);
+Node tree(1);
+tree.AddLeft(new Node(1));
+tree.GetLeft()->AddLeft(new Node(1));
+tree.GetLeft()->AddRight(new Node(1));
+tree.GetLeft()->GetRight()->AddRight(new Node(1));
+tree.AddRight(new Node(1));
+tree.GetRight()->AddRight(new Node(1));
+tree.GetRight()->GetRight()->AddRight(new Node(1));
+tree.GetRight()->GetRight()->AddLeft(new Node(1));
+
+ASSERT_THAT(analyzer.GetResult(&tree) == 1);
+END_UNIT
+
+
 TEST_SUITE(Tree)
 TEST(simple)
     TEST(empty)
+    TEST(list)
+    TEST(list1)
+    TEST(list2)
+    TEST(all_same)
 END_SUITE
